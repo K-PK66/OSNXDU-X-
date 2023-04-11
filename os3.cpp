ece1fba4bd79a59b764e88fcf67889a3984940db
Koo Pung-Kei <83063635+K-PK66@users.noreply.github.com>
2023-04-11 12:32:04 +0800

    Fourth Submit
    Finished the 3rd experiment

--- os3.cpp
+++ os3.cpp
@@ -1,24 +1,32 @@
-#include<unistd.h>
-#include<stdlib.h>
-#include<signal.h>
-#include<bits/stdc++.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <bits/stdc++.h>
 
-void sig_handle(int sig) {
-    std::cout << "get sigint\n";
+using namespace std;
+
+void sig_handle(int sig)
+{
+    cout << "Sigint get!\n";
     signal(SIGINT, SIG_DFL);
 }
 
-int main() {
+int main()
+{
     auto fa = getpid();
     auto x = fork();
-    if( getpid() == fa ) {
-        std::cout << "fa : " << getpid() << "\n";
-        if(signal(SIGINT, sig_handle) == SIG_ERR) {
-            std::cout << "qwq\n";
+    if (getpid() == fa)
+    {
+        cout << "fa : " << getpid() << "\n";
+        if (signal(SIGINT, sig_handle) == SIG_ERR)
+        {
+            cout << "ERROR!\n";
         }
         sleep(100);
-    } else {
-        std::cout << "ch : " << getpid() << "\n";
-        kill( getppid(), SIGINT );
-    } 
+    }
+    else
+    {
+        cout << "ch : " << getpid() << "\n";
+        kill(getppid(), SIGINT);
+    }
 }
